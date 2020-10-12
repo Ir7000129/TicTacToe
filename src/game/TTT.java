d04fd65a957245b3edede77201ab9c3e2a6ade8d
ir7000129 <sprider5632@gmail.com>
2020-10-13 11:01:38 +1200

    Working TTT Build

--- src/game/TTT.java
+++ src/game/TTT.java
@@ -1,10 +1,15 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TTT {
-
+	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+	static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+	
 	private static char [][] board = {{' ','|',' ', '|', ' '},
 									{'-','+','-', '+', '-'},
 									{' ','|',' ', '|', ' '},
@@ -14,18 +19,33 @@ public class TTT {
 	public static void main(String[] args) {
 		printBoard();
 		
-		System.out.println("Enter");
-		var scanner = new Scanner(System.in);
-		int playerPos = scanner.nextInt();
-		
-		readInput(board, playerPos, "player");
-		
-		var rand = new Random();
-		int cpuPos = rand.nextInt(9) + 1;
-		
-		readInput(board, cpuPos, "cpu");
-		
-		printBoard();
+		while (true) {
+			System.out.println("Enter");
+			var scanner = new Scanner(System.in);
+			int playerPos = scanner.nextInt();
+			while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPositions)) {
+				System.out.println("Position Taken!");
+				playerPos = scanner.nextInt();
+			}
+			
+			readInput(board, playerPos, "player");
+			
+			var rand = new Random();
+			int cpuPos = rand.nextInt(9) + 1;
+			while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+				cpuPos = rand.nextInt(9) + 1;
+			}
+			readInput(board, cpuPos, "cpu");			
+			printBoard();
+			String result = checkWinner();
+			System.out.println(result);
+			
+			if (checkWinner().equals("You Won!") || checkWinner().equals("You Lost!") || checkWinner().equals("Tie!")) {
+				System.out.println("Game Over");
+				break;
+			} else continue;
+		}
+	
 	}
 
 	
@@ -53,8 +73,11 @@ public class TTT {
 		
 		if(user.equals("player")) {
 			symbol = 'X';
+			playerPositions.add(pos);
 		}else if (user.equals("cpu")){
-			symbol ='O';		}
+			symbol ='O';
+			cpuPositions.add(pos);
+			}
 		switch (pos) {
 		case 1:
 			board[0][0] = symbol;
@@ -81,10 +104,47 @@ public class TTT {
 			board[4][2] = symbol;
 			break;
 		case 9:
-			board[2][4] = symbol;
+			board[4][4] = symbol;
 			break;
 		default:
 			break;
 		}
+	}
+	
+	public static String checkWinner() {
+		
+		List topRow = Arrays.asList(1, 2, 3);
+		List midRow = Arrays.asList(4, 5, 6);
+		List botRow = Arrays.asList(7, 8, 9);
+		List leftCol= Arrays.asList(1, 4, 7);
+		List midCol = Arrays.asList(2, 5, 8);
+		List rightCol = Arrays.asList(3, 6, 9);
+		List diagonal1 = Arrays.asList(1, 5, 9);
+		List diagonal2 = Arrays.asList(3, 5, 7);
+		
+		List<List> winning = new ArrayList<List>();
+		winning.add(topRow);
+		winning.add(midRow);
+		winning.add(botRow);
+		winning.add(leftCol);
+		winning.add(midCol);
+		winning.add(rightCol);
+		winning.add(diagonal1);
+		winning.add(diagonal2);
+		
+		for(List l: winning) {
+			if(playerPositions.containsAll(l)) {
+				return "You Won!";
+//				System.out.println("You Won!");
+			} else if (cpuPositions.containsAll(l)) {
+				return "You Lost!";
+//				System.out.println("You Lost!");
+			} else if(playerPositions.size() + cpuPositions.size() == 9) {
+				return "Tie";
+//				System.out.println("Tie");
+			}
+		}
+		
+		return "";
 	}
 }
